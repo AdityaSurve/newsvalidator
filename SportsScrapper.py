b3787605c43be0c60e59b16a29208bcf3feb3ed9
AdityaSurve <surveaditya521@gmail.com>
2024-06-08 16:05:07 +0530

    BCCI and IndianAthletes

---
+++ SportsScrapper.py
@@ -0,0 +1,321 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+
+
+class BCCI_Scrapper:
+    def __init__(self):
+        self.url = "https://www.bcci.tv/search?"
+        self.player_name = None
+        self.platform = None
+        self.type = None
+
+    def get_player_data(self, player_name, platform, type):
+        processed_player_name = player_name.replace(" ", "+")
+        processed_platform = platform.lower()
+        processed_type = type.lower()
+
+        if not processed_platform or not processed_type or not processed_player_name:
+            return {"Error": "Invalid input"}
+
+        self.player_name = processed_player_name
+        self.platform = processed_platform
+        self.type = processed_type
+
+        url = self.url + "platform=" + self.platform + "&type=" + \
+            self.type + "&term=" + self.player_name + "&content_type=all"
+
+        response = self.get_data(
+            url, processed_platform, processed_type, processed_player_name)
+
+        if response:
+            return {"Response": response}
+        else:
+            return {"Error": "No data found"}
+
+    def get_data(self, url, processed_platform, processed_type, player_name):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(e)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        data = []
+
+        player_data = soup.find(
+            'div', class_='lv pc video-section-append-video-here')
+        if player_data:
+            player_card = player_data.find_all(
+                'div', class_='slick-card m-0 lv-bg hoverVideoPlayNow')
+            for card in player_card:
+                header_content = card.find('div', class_='bottom')
+                title_holder = None
+                title = ""
+                date = ""
+                views = ""
+                image_url = ""
+                link = ""
+
+                if header_content.find('div', class_='text-detail br-b'):
+                    title_holder = header_content.find(
+                        'div', class_='text-detail br-b')
+                    date_holder = header_content.find(
+                        'div', class_='text-detail br-b')
+                    date = date_holder.find('span').text
+                    views_holder = header_content.find(
+                        'div', class_='tour-overlay-details')
+                    list = views_holder.find('ul')
+                    views = list.find_all('li')[0]
+                    views = views.find('span', class_="me-3").text
+                    views = views.replace("&nbsp;", "").replace(
+                        "views", "").replace("\n", "").replace("\xa0", "").strip()
+                    if "k" in views:
+                        views = views.replace("k", "")
+                        views = float(views) * 1000
+                    elif "m" in views:
+                        views = views.replace("m", "")
+                        views = float(views) * 1000000
+                    views = int(views)
+
+                    links_holder = card.find('a')
+                    link = links_holder['data-share']
+                    image_url = links_holder['data-thumbnile']
+
+                elif header_content.find('div', class_='text-detail pb-0'):
+                    title_holder = header_content.find(
+                        'div', class_='text-detail pb-0')
+                    date_holder = header_content.find(
+                        'div', class_='tour-overlay-details')
+                    ul = date_holder.find('ul')
+                    date_ = ul.find_all('li')[0]
+                    date = date_.find('span').text
+                    views = "-"
+                    links_holder = card.find('a')
+                    link = links_holder['href']
+                    link = "https://www.bcci.tv" + link
+                    image_url = links_holder['data-thumbnile']
+                if title_holder:
+                    title = title_holder.find('p').text
+                temp_player_name = player_name.replace("+", " ")
+                data.append({
+                    "title": title,
+                    "date": date,
+                    "views": views,
+                    "platform": processed_platform,
+                    "type": processed_type,
+                    "player_name": temp_player_name,
+                    "image_url": image_url,
+                    "link": link,
+                    "sport": "Cricket"
+                })
+            if data:
+                df = pd.DataFrame(data)
+                header = ['title', 'date', 'views',
+                          'platform', 'type', 'player_name', 'image_url', 'link', 'sport']
+                if os.path.isfile('BCCI.csv') and os.path.getsize('BCCI.csv') > 0:
+                    df.to_csv('BCCI.csv', mode='a',
+                              header=False, index=False)
+                else:
+                    df.to_csv('BCCI.csv', mode='a',
+                              header=header, index=False)
+                return data
+            else:
+                return {"Error": "No data found"}
+        else:
+            return {"Error": "No data found"}
+
+
+class Indian_Athletes_Scrapper:
+    def __init__(self):
+        self.url = "https://indianathletics.in/?"
+        self.player_name = None
+
+    def get_player_data(self, player_name):
+        processed_player_name = player_name.replace(" ", "+")
+
+        if not processed_player_name:
+            return {"Error": "Invalid input"}
+
+        self.player_name = processed_player_name
+
+        url = self.url + "s=" + self.player_name
+
+        response = self.get_data(processed_player_name, url)
+
+        if response:
+            return {"Response": response}
+        else:
+            return {"Error": "No data found"}
+
+    def get_data(self, player_name, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(e)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        data = []
+
+        player_data = soup.find('ol', class_='search_res')
+        if player_data:
+            title = ""
+            date = ""
+            content = ""
+            image_url = ""
+            link = ""
+
+            li_tags = player_data.find_all('li')
+            for li in li_tags:
+                divs_holder = li.find('div')
+                title_holder = divs_holder.find('h3')
+                title = title_holder.find('a')['title']
+                title = title.replace("Permalink to ", "")
+                link = title_holder.find('a')['href']
+
+                home_page = requests.get(link)
+                home_page.raise_for_status()
+                home_soup = BeautifulSoup(home_page.text, 'html.parser')
+                content_holder = home_soup.find(
+                    'div', class_='post_content col span_3_of_4')
+                if content_holder:
+                    content_holder = content_holder.find(
+                        'div', class_="section")
+                    image_holder = content_holder.find(
+                        'div', class_='pic')
+                    if image_holder:
+                        image_holder = image_holder.find('a')
+                    if image_holder:
+                        image_url = image_holder['href']
+
+                    date_holder = content_holder.find(
+                        'p', class_='post_meta')
+                    date_span = date_holder.find('span')
+                    date = date_span.text
+
+                    content = content_holder.find(
+                        'div', class_="post_description")
+
+                    text = ''
+                    if content:
+                        content = content.find_all('p')
+                        for c in content:
+                            text += c.text + "\n\n"
+                        text = text.strip()
+                    temp_player_name = player_name.replace("+", " ")
+                    data.append({
+                        "title": title,
+                        "date": date,
+                        "content": text,
+                        "player_name": temp_player_name,
+                        "image_url": image_url,
+                        "link": link,
+                        "sport": "Athletics"
+                    })
+            if data:
+                df = pd.DataFrame(data)
+                header = ['title', 'date', 'content',
+                          'player_name', 'image_url', 'link', 'sport']
+                if os.path.isfile('IndianAthletes.csv') and os.path.getsize('IndianAthletes.csv') > 0:
+                    df.to_csv('IndianAthletes.csv', mode='a',
+                              header=False, index=False)
+                else:
+                    df.to_csv('IndianAthletes.csv', mode='a',
+                              header=header, index=False)
+                return data
+            else:
+                return {"Error": "No data found"}
+        else:
+            return {"Error": "No data found"}
+
+
+if __name__ == "__main__":
+    # scrapper = BCCI_Scrapper()
+    # platform = "International"
+    # type = "Men"
+
+    # player_name = "Virat Kohli"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Sachin Tendulkar"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Rohit Sharma"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "MS Dhoni"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Sourav Ganguly"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Ravindra Jadeja"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Jasprit Bumrah"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Yuvraj Singh"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Harbhajan Singh"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Irfan Pathan"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Zaheer Khan"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Bumrah"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Jadeja"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # type = "Women"
+
+    # player_name = "Mithali Raj"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Harmanpreet Kaur"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Smriti Mandhana"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Jhulan Goswami"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Poonam Yadav"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Shafali Verma"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Deepti Sharma"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    # player_name = "Veda Krishnamurthy"
+    # scrapper.get_player_data(player_name, platform, type)
+
+    scrapper = Indian_Athletes_Scrapper()
+    player_name = "Neeraj Chopra"
+    scrapper.get_player_data(player_name)
+
+    player_name = "Hima Das"
+    scrapper.get_player_data(player_name)
+
+    player_name = "Dutee Chand"
+    scrapper.get_player_data(player_name)
+
+    player_name = "Anju Bobby George"
+    scrapper.get_player_data(player_name)
+
+    player_name = "Usha"
+    scrapper.get_player_data(player_name)
+
+    player_name = "Milkha Singh"
+    scrapper.get_player_data(player_name)
--- server.py
+++ server.py
@@ -1,248 +1,34 @@
 from flask import Flask, request, jsonify
-from DataIngestion import NewsApi
 from flask_cors import CORS
-from sklearn.feature_extraction.text import TfidfVectorizer
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.cluster import AgglomerativeClustering
-import re
-from sklearn.metrics.pairwise import linear_kernel
-from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
-from scipy.spatial.distance import cosine
-import torch
-import spacy
-from DataModelling import DataModel
 import pandas as pd
-
-BOOST_VALUE = 0.2
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-model = AutoModel.from_pretrained('bert-base-uncased')
-model_truthValue = AutoModelForSequenceClassification.from_pretrained(
-    "textattack/bert-base-uncased-SST-2")
-tokenizer_truthValue = AutoTokenizer.from_pretrained(
-    "textattack/bert-base-uncased-SST-2")
-nlp = spacy.load("en_core_web_sm")
-data_model = DataModel()
-
-def get_main_verb(sentence):
-    doc = nlp(sentence)
-    main_verb = None
-    for token in doc:
-        if token.pos_ == "VERB" and token.dep_ != "aux":
-            main_verb = token.text
-            break
-    return main_verb
-
-def get_truth_value(article):
-    title = article['title'] if article['title'] is not None else ''
-    description = article['description'] if article['description'] is not None else ''
-    text = title + ' ' + description
-    inputs = tokenizer_truthValue(text, truncation=True,
-                                  padding=True, return_tensors='pt')
-    outputs = model_truthValue(**inputs)
-    probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)
-    truth_value = probabilities[0][1].item()
-    return truth_value
-
-def calculate_similarity(vector1, vector2):
-    return linear_kernel(vector1, vector2).flatten()[0]
-
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-
-def preprocess_articles(articles):
-    stop_words = set(stopwords.words('english'))
-    lemmatizer = WordNetLemmatizer()
-    preprocessed_articles = []
-    for article in articles:
-        if article is None or article.strip() == '':
-            continue
-        article = re.sub(r'\s+', ' ', article)
-        article = re.sub(r'[^\w\s]', '', article)
-        article = article.lower()
-
-        tokens = word_tokenize(article)
-        tokens = [token for token in tokens if token not in stop_words]
-
-        lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
-        preprocessed_articles.append(' '.join(lemmatized_tokens))
-
-    return preprocessed_articles
-
-
-def group_similar_articles(articles):
-    preprocessed_articles = preprocess_articles(
-        [article['content'] for article in articles])
-
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(preprocessed_articles)
-
-    similarity_matrix = cosine_similarity(X)
-
-    clustering_model = AgglomerativeClustering(
-        n_clusters=None, affinity='precomputed', linkage='average', distance_threshold=0.5)
-    labels = clustering_model.fit_predict(1 - similarity_matrix)
-
-    grouped_articles = {}
-    for i, label in enumerate(labels):
-        if label not in grouped_articles:
-            grouped_articles[label] = []
-        grouped_articles[label].append(articles[i])
-
-    return grouped_articles
-
-def summarize_cluster(articles):
-    if len(articles) == 0:
-        return {
-            'numberOfArticles': 0,
-            'firstArticle': None,
-            'lastArticle': None,
-            'sources': [],
-            'descriptionGroups': ""
-        }
-    if len(articles) < 2:
-        return {
-            'numberOfArticles': len(articles),
-            'firstArticle': articles[0],
-            'lastArticle': articles[0],
-            'sources': [articles[0]['source']['name']],
-            'description': articles[0]['description']
-        }
-    descriptions = [article['description'] for article in articles]
-    preprocessed_descriptions = preprocess_articles(descriptions)
-    if len(preprocessed_descriptions) == 0:
-        return {
-            'numberOfArticles': len(articles),
-            'firstArticle': articles[0],
-            'lastArticle': articles[-1],
-            'sources': [article['source']['name'] for article in articles],
-            'description': descriptions[0]
-        }
-    sources = [article['source']['name'] for article in articles]
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(preprocessed_descriptions)
-    similarity_matrix = cosine_similarity(X)
-    clustering_model = AgglomerativeClustering(
-        n_clusters=None, affinity='precomputed', linkage='average', distance_threshold=0.5)
-    labels = clustering_model.fit_predict(1 - similarity_matrix)
-
-    description = ""
-    for i, label in enumerate(labels):
-        label = int(label)
-        description = descriptions[i]
-
-    return {
-        'numberOfArticles': len(articles),
-        'firstArticle': articles[0],
-        'lastArticle': articles[-1],
-        'sources': sources,
-        'description': description,
-    }
-
-
-def get_embedding(text):
-    inputs = tokenizer(text, return_tensors='pt')
-    outputs = model(**inputs)
-    return outputs.last_hidden_state.mean(dim=1).squeeze().detach().numpy()
+from SportsScrapper import BCCI_Scrapper
+from SportsScrapper import Indian_Athletes_Scrapper
 
 app = Flask(__name__)
 CORS(app)
-newsapi = NewsApi()
-
-@app.route('/newsapi', methods=['GET'])
-def get_newsapi():
-    category = request.args.get('category')
-    source = request.args.get('source')
-    query = request.args.get('query')
-    language = request.args.get('language')
-    country = request.args.get('country')
-    date = request.args.get('date')
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
-    news = newsapi.get_news(category, source, query,
-                            language, country, date, start_date, end_date)
-    if len(news['articles']) == 0:
-        return jsonify({'error': 'No articles found'})
-    if len(news['articles']) < 5:
-        return jsonify(news)
-    query_embedding = get_embedding(query)
-    for i, article in enumerate(news['articles']):
-        description = article['description']
-        title = article['title']
-        if description is not None and description.strip() != '':
-            description_embedding = get_embedding(description)
-            similarity_desc = 1 - \
-                cosine(query_embedding, description_embedding)
-        if title is not None and title.strip() != '':
-            title_embedding = get_embedding(title)
-            similarity_title = 1 - cosine(query_embedding, title_embedding)
-        avg_similarity = min(
-            ((similarity_desc + similarity_title) / 2) + BOOST_VALUE, 1)
-        news['articles'][i]['relevanceScore'] = avg_similarity
-        news['articles'][i]['truthValue'] = get_truth_value(article)
-    filtered_articles = [i for i in news['articles']
-                         if 'relevanceScore' in i and i['relevanceScore'] > 0.5]
-    filtered_articles = sorted(
-        filtered_articles, key=lambda x: x['relevanceScore'], reverse=True)
-    news['articles'] = filtered_articles
-    analysis = []
-    grouped_articles = group_similar_articles(news['articles'])
-    for articles in grouped_articles.values():
-        analysis.append(summarize_cluster(articles))
-    news['analysis'] = analysis if len(analysis) > 0 else []
-
-    for article in news['articles']:
-        if article['relevanceScore'] > 0.8 and article['truthValue'] > 0.9:
-            title = article["title"]
-            main_verb = get_main_verb(title)
-            custom_pattern = re.compile(
-                fr'(?P<entity1>.+?)\s+(?P<relation>\b(?:{main_verb})\b)\s+(?P<entity2>.+)')
-            match = custom_pattern.match(title)
-            if match is not None:
-                entity1 = match.group("entity1").strip()
-                entity2 = match.group("entity2").strip()
-                relation = match.group("relation").strip()
-                data_model.add_entity(entity1)
-                data_model.add_entity(entity2)
-                data_model.add_relation(relation, [
-                    entity1,
-                    entity2
-                ])
-    data_model.save_to_csv()
-    data_model.preprocess()
-    data_model.print()
-    return jsonify(news)
 
 
-def get_most_similar_entity(entity, entities):
-    entity_doc = nlp(entity)
-    similarity_scores = []
-    for ent in entities:
-        ent_doc = nlp(ent)
-        similarity_scores.append(entity_doc.similarity(ent_doc))
-    most_similar_index = similarity_scores.index(max(similarity_scores))
-    most_similar_entity = entities[most_similar_index]
-    return most_similar_entity
+@app.route('/bcci', methods=['GET'])
+def get_bcci():
+    player_name = request.args.get('player_name')
+    platform = request.args.get('platform')
+    type = request.args.get('type')
 
-@app.route('/graph', methods=['GET'])
-def get_entities_and_relations():
-    data_model.preprocess_relations()
-    entity = request.args.get('entity')
-    most_similar_entity = get_most_similar_entity(
-        entity, data_model.get_all_entities())
-    r = data_model.get_all_relations_by_entity(most_similar_entity)
-    if len(r) > 0:
-        r_df = pd.DataFrame(r)
-        return jsonify(r_df.to_dict(orient='records'))
-    else:
-        return []
+    scrapper = BCCI_Scrapper()
+    articles = scrapper.get_player_data(player_name, platform, type)
+
+    return jsonify(articles)
+
+
+@app.route('/indianathletics', methods=['GET'])
+def get_indianathletics():
+    player_name = request.args.get('player_name')
+
+    scrapper = Indian_Athletes_Scrapper()
+    articles = scrapper.get_player_data(player_name)
+
+    return jsonify(articles)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
