52be1a7f54a2877f88504ea4a880dfdb03b9f41d
AdityaSurve <surveaditya521@gmail.com>
2024-06-17 02:53:03 +0530

    truth value

--- NewsVerification.py
+++ NewsVerification.py
@@ -13,14 +13,19 @@ from nltk.stem import WordNetLemmatizer
 import string
 from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch.nn.functional as F
+import torch
 
 class Validator:
     def __init__(self):
-        self.sentiment_pipeline = pipeline('sentiment-analysis')
-        self.political_influence_model = pipeline(
-            'text-classification', model='typeform/distilbert-base-uncased-mnli')
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.model = BertModel.from_pretrained('bert-base-uncased')
+        self.sentiment_pipeline = pipeline("sentiment-analysis")
+        self.emotion_pipeline = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+        political_model_name = "premsa/political-bias-prediction-allsides-mDeBERTa"
+        self.political_model = AutoModelForSequenceClassification.from_pretrained(political_model_name)
+        self.political_tokenizer = AutoTokenizer.from_pretrained(political_model_name)
 
     def bert_embedding(self, text):
         inputs = self.tokenizer(text, return_tensors='pt',
@@ -189,99 +194,127 @@ class Validator:
         return data
 
     def assess_truth(self, unofficial_data, official_data):
+        official_embeddings = []
+        for official_article in official_data:
+            title = unidecode(official_article['title'])
+            content = unidecode(official_article['content'])
+            description = unidecode(official_article['description'])
+            official_text = f"{title} {content} {description}"
+            official_embeddings.append(self.bert_embedding(official_text).squeeze())
         truth_values = []
-        vectorizer = TfidfVectorizer()
+        unofficial_embeddings = []
         for article in unofficial_data:
             unofficial_text = f"{article['title']} {article['description']} {article['content']} {article['summary']}"
-            similarity_scores = []
-            for official_article in official_data:
-                title = unidecode(official_article['title'])
-                content = unidecode(official_article['content'])
-                description = unidecode(official_article['description'])
-                official_text = f"{title} {content} {description}"
-                vectors = vectorizer.fit_transform(
-                    [official_text, unofficial_text])
-                similarity = cosine_similarity(
-                    vectors[0:1], vectors[1:2])[0][0]
-                similarity_scores.append(similarity)
+            unofficial_embeddings.append(self.bert_embedding(unofficial_text).squeeze())
+        official_embeddings = np.array(official_embeddings)
+        unofficial_embeddings = np.array(unofficial_embeddings)
+        for unofficial_embedding in unofficial_embeddings:
+            similarity_scores = cosine_similarity([unofficial_embedding], official_embeddings)[0]
             truth_value = max(similarity_scores)
             truth_values.append(truth_value)
-        return truth_values
+        unofficial_similarity_matrix = cosine_similarity(unofficial_embeddings)
+        consensus_scores = unofficial_similarity_matrix.mean(axis=1)
+        combined_scores = [(truth_value + consensus_score) / 2 for truth_value, consensus_score in zip(truth_values, consensus_scores)]
+
+        return combined_scores
 
     def detect_influence(self, article):
         content = article['content']
         sentiment_result = self.sentiment_pipeline(content)
-        emotional_influence = sentiment_result[0]['label'] in [
-            'NEGATIVE', 'POSITIVE']
-        political_result = self.political_influence_model(content)
-        political_influence = any(
-            label['label'] == 'POLITICS' and label['score'] > 0.5 for label in political_result)
+        emotional_influence = sentiment_result[0]['label'] in ['NEGATIVE', 'POSITIVE']
+        political_inputs = self.political_tokenizer.encode_plus(content, return_tensors='pt', truncation=True, padding=True)
+        political_output = self.political_model(**political_inputs)
+        political_probabilities = torch.nn.functional.softmax(political_output.logits, dim=-1)
+        political_labels = ['Neutral', 'Liberal', 'Conservative']
+        political_scores = political_probabilities.detach().numpy()[0]
+        political_influence = any(score > 0.5 for score in political_scores if political_labels[political_scores.tolist().index(score)] != 'Neutral')
+        
         return political_influence, emotional_influence
 
     def sentiment_analysis(self, article):
-        analysis = TextBlob(article['content'])
-        polarity = analysis.sentiment.polarity
-        subjectivity = analysis.sentiment.subjectivity
-        if polarity > 0:
-            sentiment = 'Positive, which means the text is expressing positive emotions or opinions'
-        elif polarity < 0:
-            sentiment = 'Negative, which means the text is expressing negative emotions or opinions'
+        if 'content' in article and article['content'] is not None:
+            text = article['content']
+            analysis = TextBlob(text)
+            subjectivity = analysis.sentiment.subjectivity
+            if 'sentiment' not in article or article['sentiment'] is None:
+                article['sentiment'] = None
+            if article['sentiment'] is None:
+                model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+                model = AutoModelForSequenceClassification.from_pretrained(model_name)
+                tokenizer = AutoTokenizer.from_pretrained(model_name)
+                inputs = tokenizer.encode_plus(text, return_tensors='pt', truncation=True, padding=True)
+                output = model(**inputs)
+                probabilities = F.softmax(output.logits, dim=-1)
+                sentiment_values = probabilities.detach().numpy()[0]
+                article['sentiment'] = {}
+                article['sentiment']['negative'] = round(float(sentiment_values[0]), 6)
+                article['sentiment']['neutral'] = round(float(sentiment_values[1]), 6)
+                article['sentiment']['positive'] =  round(float(sentiment_values[2]), 6)
+            if subjectivity >= 0.5:
+                objectivity = 'Subjective'
+            else:
+                objectivity = 'Objective'
+            sentiment_polarity_labels = ['Negative', 'Neutral', 'Positive']
+            sentiment_polarity_scores = [article['sentiment']['negative'], article['sentiment']['neutral'], article['sentiment']['positive']]
+            max_sentiment_polarity_score = max(sentiment_polarity_scores)
+            sentiment_polarity_label = sentiment_polarity_labels[sentiment_polarity_scores.index(max_sentiment_polarity_score)]
+            return {
+                'polarity_label': sentiment_polarity_label,
+                'subjectivity': subjectivity,
+                'subjectivity_label': objectivity
+            }
         else:
-            sentiment = 'Neutral, which means the text is neither positive nor negative'
-        if subjectivity >= 0.5:
-            objectivity = 'Subjective, which means the text is based on opinions or beliefs'
-        else:
-            objectivity = 'Objective, which means the text is based on facts or evidence'
-
-        return {
-            'polarity': polarity,
-            'polarity_label': sentiment,
-            'subjectivity': subjectivity,
-            'subjectivity_label': objectivity
-        }
+            return None
 
     def relevance_score(self, article, query):
-        def preprocess_text(text):
-            if text is None:
-                return ''
-            lemmatizer = WordNetLemmatizer()
-            text = text.lower()
-            text = "".join(
-                [char for char in text if char not in string.punctuation])
-            words = word_tokenize(text)
-            words = [lemmatizer.lemmatize(word)
-                     for word in words if word not in ENGLISH_STOP_WORDS]
-            return " ".join(words)
+        # def preprocess_text(text):
+        #     if text is None:
+        #         return ''
+        #     lemmatizer = WordNetLemmatizer()
+        #     text = text.lower()
+        #     text = "".join(
+        #         [char for char in text if char not in string.punctuation])
+        #     words = word_tokenize(text)
+        #     words = [lemmatizer.lemmatize(word)
+        #              for word in words if word not in ENGLISH_STOP_WORDS]
+        #     return " ".join(words)
 
-        def get_word2vec_embeddings(text):
-            words = word_tokenize(text)
-            model = Word2Vec([words], min_count=1)
-            embeddings = np.mean([model.wv[word] for word in words], axis=0)
-            return embeddings.reshape(1, -1)
+        # def get_word2vec_embeddings(text):
+        #     words = word_tokenize(text)
+        #     model = Word2Vec([words], min_count=1)
+        #     embeddings = np.mean([model.wv[word] for word in words], axis=0)
+        #     return embeddings.reshape(1, -1)
 
-        title = preprocess_text(article['title'])
-        description = preprocess_text(article['description'])
-        content = preprocess_text(article['content'])
-        combined_text = f"{title} {description} {content}"
-        query = preprocess_text(query)
+        # title = preprocess_text(article['title'])
+        # description = preprocess_text(article['description'])
+        # content = preprocess_text(article['content'])
+        # combined_text = f"{title} {description} {content}"
+        # query = preprocess_text(query)
 
-        vectorizer = TfidfVectorizer()
-        vectors = vectorizer.fit_transform([query, combined_text])
-        tfidf_similarity = cosine_similarity(vectors[0:1], vectors[1:2])[0][0]
+        # vectorizer = TfidfVectorizer()
+        # vectors = vectorizer.fit_transform([query, combined_text])
+        # tfidf_similarity = cosine_similarity(vectors[0:1], vectors[1:2])[0][0]
 
-        query_embedding = self.bert_embedding(query)
-        text_embedding = self.bert_embedding(combined_text)
-        bert_similarity = cosine_similarity(
-            query_embedding, text_embedding)[0][0]
+        # query_embedding = self.bert_embedding(query)
+        # text_embedding = self.bert_embedding(combined_text)
+        # bert_similarity = cosine_similarity(
+        #     query_embedding, text_embedding)[0][0]
 
-        query_embedding_w2v = get_word2vec_embeddings(query)
-        text_embedding_w2v = get_word2vec_embeddings(combined_text)
-        w2v_similarity = cosine_similarity(
-            query_embedding_w2v, text_embedding_w2v)[0][0]
+        # query_embedding_w2v = get_word2vec_embeddings(query)
+        # text_embedding_w2v = get_word2vec_embeddings(combined_text)
+        # w2v_similarity = cosine_similarity(
+        #     query_embedding_w2v, text_embedding_w2v)[0][0]
 
-        similarity = np.average(
-            [bert_similarity, tfidf_similarity, w2v_similarity], weights=[0.5, 0.3, 0.2])
+        # similarity = np.average(
+        #     [bert_similarity, tfidf_similarity, w2v_similarity], weights=[0.5, 0.3, 0.2])
 
+        # return {
+        #     'similarity': similarity
+        # }
+        article_text = f"{article['title']} {article['description']} {article['content']} {article['summary']}"
+        article_embedding = self.bert_embedding(article_text).squeeze()
+        query_embedding = self.bert_embedding(query).squeeze()
+        similarity = cosine_similarity([article_embedding], [query_embedding])[0][0]
         return {
             'similarity': similarity
         }
@@ -306,7 +339,6 @@ class Validator:
         for i, article in enumerate(unofficial_data):
             article['truth_value'] = truth_values[i]
             article['political_influence'], article['emotional_influence'] = influences[i]
-            article['sentiment_polarity'] = sentiments[i]['polarity']
             article['sentiment_polarity_label'] = sentiments[i]['polarity_label']
             article['sentiment_subjectivity'] = sentiments[i]['subjectivity']
             article['sentiment_subjectivity_label'] = sentiments[i]['subjectivity_label']
